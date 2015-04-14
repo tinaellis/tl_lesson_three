456db88cd56fb88efe92b6f2df0edc246f7ec762
Tina Ellis <tina.ellis@getg5.com>
2015-04-14 05:06:20 -0700

    create files and nested files

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -11,3 +11,11 @@ use Rack::Session::Cookie, :key => 'rack.session',
 get '/' do
   "Good morning World!"
 end
+
+get '/spiderman' do
+  erb :spiderman
+end
+
+post '/forms/login' do
+  erb :forms
+end
---
+++ tl-blackjack-webapp/views/forms/login.rb
@@ -0,0 +1,4 @@
+<form action=‘/myaction’ method=‘post’>
+  <input type=‘text’ name=‘username’/>
+  <input type=‘submit’ class=‘btn’/>
+</form>
