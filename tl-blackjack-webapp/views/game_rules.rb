af1e216c4b8cc4edd91eabb611ed9ef0e8e0e8b8
Tina Ellis <tina.ellis@getg5.com>
2015-04-14 05:12:23 -0700

    Translate notes into game plan

--- tl-blackjack-webapp/views/game_rules.rb
+++ tl-blackjack-webapp/views/game_rules.rb
@@ -1,16 +1,22 @@
-ask for name, create form to capture information
-create session out of name
-allocate “500” dollars to the user
-begin loop
-ask for bet amount
-play game
-- deal one to dealer, one is face down
-- deal two to user
-- check for blackjack
-- hit or stay
-- if hit deal another card
-- if stay - compare card to dealers card
-- if won, puts won, if lose, puts lose, if tie, puts tie
-check remaining balance, put balance
-if 0 puts game over and end loop
-play again?
+# FORM
+# ask for name, create form to capture information
+# create session out of name
+
+# MONEY
+# allocate “500” dollars to the user
+# create session to remember number amount
+
+# # GAME LOOP
+# begin loop
+# ask for bet amount (refer to money session)
+# play game
+    # - deal one to dealer, one is face down
+    # - deal two to user
+    # - check for blackjack
+    # - hit or stay
+    # - if hit deal another card
+    # - if stay - compare card to dealers card
+    # - if won, puts won, if lose, puts lose, if tie, puts tie
+    # check remaining balance, put balance (refer to money session)
+    # if 0 puts game over and end loop
+# play again?
